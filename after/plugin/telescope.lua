41d897c796a4f9f359b5463e078ece0926d61e46
sirdigbot <sirdigbot@users.noreply.github.com>
2023-05-08 16:28:31 +1000

    Fix grep keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,8 +4,10 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope: Search the 
 vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = '[T]elescope: [K]eymaps' })
 
 -- grep_string requires ripgrep to be installed (rg command)
-vim.keymap.set('n', '<leader>pg', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end,
-{ desc = 'Telescope: [P]roject [G]rep' })
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope: Live [P]roject [G]rep' })
 
+-- Non-live version
+vim.keymap.set('n', '<leader>ps', function()
+        builtin.grep_string( --[[ { search = vim.fn.input('Grep > ') } ]])
+    end,
+    { desc = 'Telescope: [P]roject [S]earch for word/string under cursor' })
