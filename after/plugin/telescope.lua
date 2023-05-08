077419d2c4d259f917c077a0b5d7fb9475a8facd
sirdigbot <sirdigbot@users.noreply.github.com>
2023-05-08 16:42:32 +1000

    Add keymap for telescope.builtin.resume

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,12 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope: Search [P]roject [F]iles' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope: Search the output of \'git ls-files\'' })
-vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = '[T]elescope: [K]eymaps' })
+vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = 'Telescope: [K]eymaps' })
 
--- grep_string requires ripgrep to be installed (rg command)
+vim.keymap.set('n', '<leader>pr', builtin.resume, { desc = 'Telescope: Show/Resume [P]revious telescope picker [R]esults' })
+
+-- Grep capabilities depend on ripgrep ("rg" must be a valid terminal command).
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope: Live [P]roject [G]rep' })
-
--- Non-live version
 vim.keymap.set('n', '<leader>ps', function()
         builtin.grep_string( --[[ { search = vim.fn.input('Grep > ') } ]])
     end,
