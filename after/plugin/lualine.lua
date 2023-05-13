b46a7ffd1eff4fcacaf7ae0bffe78de81e1e4be8
sirdigbot <sirdigbot@users.noreply.github.com>
2023-05-13 14:56:17 +1000

    Improve lualine text

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,41 +1,60 @@
-require('lualine').setup {
-  options = {
-    icons_enabled = false,
-    theme = 'ayu_dark',
-    component_separators = { left = '<', right = '>'},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
+-- Display shiftwidth/expandtab settings. Pairs nicely with vim-sleuth
+local function display_indent_settings()
+    local indent_type = (vim.opt.expandtab:get()) and 'spaces' or 'tabs'
+    return string.format('%d %s', vim.opt.shiftwidth:get(), indent_type)
+end
 
+-- Display more readable EOL type
+local function display_line_ending_type()
+    local type = vim.bo.fileformat
+    if type == 'unix' then
+        return 'LF'
+    elseif type == 'dos' then
+        return 'CRLF'
+    elseif type == 'mac' then
+        return 'CR'
+    end
+    return 'EOL?'
+end
+
+
+require('lualine').setup {
+    options = {
+        icons_enabled = false,
+        theme = 'ayu_dark',
+        component_separators = { left = '<', right = '>' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { display_indent_settings, 'encoding', display_line_ending_type, 'filetype' },
+        lualine_y = { 'progress', 'selectioncount' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {}
+}
